0de7c945baaa25d90c6469f4118fd1a9e286443f
Alexandre Sarfati <alexandre@docduo.com>
2025-03-06 12:01:12 +0100

    impr: update pricing and stripe ids

--- packages/utils/src/constants/pricing.tsx
+++ packages/utils/src/constants/pricing.tsx
@@ -127,7 +127,7 @@ export const PLANS: any[] = [
       yearly: 0,
     },
     limits: {
-      links: 3,
+      links: 10,
       clicks: 1000,
       sales: 0,
       domains: 3,
@@ -188,12 +188,10 @@ export const PLANS: any[] = [
       monthly: 24,
       yearly: 19,
       ids: [
-        "price_1LodNLAlJJEpqkPVQSrt33Lc", // old monthly
-        "price_1LodNLAlJJEpqkPVRxUyCQgZ", // old yearly
-        "price_1OTcQBAlJJEpqkPViGtGEsbb", // new monthly (test)
-        "price_1OYJeBAlJJEpqkPVLjTsjX0E", // new monthly (prod)
-        "price_1OTcQBAlJJEpqkPVYlCMqdLL", // new yearly (test)
-        "price_1OYJeBAlJJEpqkPVnPGEZeb0", // new yearly (prod)
+        "price_1QxoOlBN5sOoOmBUWpZhqLua", // new monthly (test)
+        "price_1QxoMQBN5sOoOmBU7JByAg4s", // new monthly (prod)
+        "price_1QxoOyBN5sOoOmBUKEy5qXku", // new yearly (test)
+        "price_1QxoNcBN5sOoOmBUxsG7P0ZZ", // new yearly (prod)
       ],
     },
     limits: {
@@ -208,8 +206,8 @@ export const PLANS: any[] = [
       api: 600,
     },
     colors: {
-      bg: "bg-blue-500",
-      text: "text-blue-500",
+      bg: "bg-[#B3E4FF]",
+      text: "text-[#08272E]",
     },
     cta: {
       text: "Get started with Pro",
@@ -264,13 +262,10 @@ export const PLANS: any[] = [
     folders: 10,
     users: 15,
     ids: [
-      "price_1LodLoAlJJEpqkPV9rD0rlNL", // old monthly
-      "price_1LodLoAlJJEpqkPVJdwv5zrG", // oldest yearly
-      "price_1OZgmnAlJJEpqkPVOj4kV64R", // old yearly
-      "price_1OzNlmAlJJEpqkPV7s9HXNAC", // new monthly (test)
-      "price_1OzNmXAlJJEpqkPVYO89lTdx", // new yearly (test)
-      "price_1OzOFIAlJJEpqkPVJxzc9irl", // new monthly (prod)
-      "price_1OzOXMAlJJEpqkPV9ERrjjbw", // new yearly (prod)
+      "price_1QxoPiBN5sOoOmBUwajH8iug", // new monthly (test)
+      "price_1QxoQ7BN5sOoOmBUgyTo0MHA", // new yearly (test)
+      "price_1QxoQiBN5sOoOmBUBKeD5OCA", // new monthly (prod)
+      "price_1QxoQwBN5sOoOmBUjUL0V39C", // new yearly (prod)
     ],
   }),
   // BUSINESS_PLAN_MODIFIER({
