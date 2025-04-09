7b26cd6801e44205b4fc1f66e8e87ed3be7a5425
Alexandre Sarfati <alexandre@docduo.com>
2025-04-10 01:09:32 +0200

    feat: add stripe apps

--- packages/utils/src/constants/integrations.ts
+++ packages/utils/src/constants/integrations.ts
@@ -1,6 +1,6 @@
 export const SLACK_INTEGRATION_ID = "clzu59rx9000110bm5fnlzwuj";
-export const STRIPE_INTEGRATION_ID = "clzra1ya60001wnj4a89zcg9h";
+export const STRIPE_INTEGRATION_ID = process.env.STRIPE_INTEGRATION_ID!;
 export const SEGMENT_INTEGRATION_ID = "int_zGnSElTzimbz20OWnXerPoKv";
 export const ZAPIER_INTEGRATION_ID = "int_MioER0SCDa6fNfaSGwrGoirM";
 export const SHOPIFY_INTEGRATION_ID = "int_iWOtrZgmcyU6XDwKr4AYYqLN";
-export const CALENDLY_INTEGRATION_ID = process.env.CALENDLY_INTEGRATION_ID;
+export const CALENDLY_INTEGRATION_ID = process.env.CALENDLY_INTEGRATION_ID!;
